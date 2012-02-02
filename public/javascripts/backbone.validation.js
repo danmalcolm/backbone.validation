ddb65d7bf90a6b1b39ec95c914bc118306f9ef51
Dan Malcolm <danmalc@gmail.com>
2012-02-02 23:12:55 +0000

    Adding message creation functionality and a bit of refactoring

--- public/javascripts/backbone.validation.js
+++ public/javascripts/backbone.validation.js
@@ -2,48 +2,7 @@ Backbone.validation = {};
 
 Backbone.validation = (function () {
 
-	var ModelValidator = function () {
-		this.attrValidators = [];
-	};
-	_.extend(ModelValidator.prototype, {
-		validate: function (model) {
-			var results = _.map(this.attrValidators, function (attrValidator) {
-				return attrValidator.validate(model);
-			});
-			results = _.filter(results, function (r) { return r != null; });
-			var isValid = results.length === 0;
-			return { isValid: isValid, results: results };
-		},
-		validateAttrs: function (attrs) {
-			var results = _.map(attrs, function (value, name) {
-				var validator = _.find(this.attrValidators, function (v) { return v.name === name; });
-				return validator ? validator.validateValue(value) : null;
-			}, this);
-			results = _.filter(results, function (r) { return r != null; });
-			var isValid = results.length === 0;
-			return { isValid: isValid, results: results };
-		},
-
-		attr: function (name) {
-			var attrValidator = _.find(this.attrValidators, function (v) { return v.name === name; });
-			if (!attrValidator) {
-				attrValidator = new AttrValidator(name);
-				this.attrValidators.push(attrValidator);
-			}
-			return attrValidator;
-		}
-	});
-
-	var Rule = function (isValid, options) {
-		this.isValid = isValid;
-		this.message = options.message;
-	};
-
-	var AttrValidator = function (name) {
-		this.name = name;
-		this.rules = [];
-	};
-
+	// Utility check functions
 	var isNullOrUndefined = function (value) {
 		return _.isNull(value) || _.isUndefined(value);
 	};
@@ -55,67 +14,201 @@ Backbone.validation = (function () {
 		return value.toString();
 	};
 
+	var areEqualIgnoreCase = function (value1, value2) {
+		if (isNullOrUndefined(value1) || isNullOrUndefined(value2))
+			return false;
+		return value1 === value2 || asString(value1).toLowerCase() === asString(value2).toLowerCase();
+	};
+
 	var trimString = function (value) {
 		return asString(value).replace(/^\s+|\s+$/g, "");
+	};
+
+
+
+	// ModelValidator - validates at model level
+	var ModelValidator = function () {
+		this.attrValidators = [];
+	};
+
+	_.extend(ModelValidator.prototype, {
+		// Validate all model attrs
+		validate: function (model) {
+			var results = _.map(this.attrValidators, function (attrValidator) {
+				return attrValidator.validate(model);
+			});
+			results = _.filter(results, function (r) { return r != null; });
+			var isValid = results.length === 0;
+			return { isValid: isValid, results: results };
+		},
+		// Just validate the supplied attributes
+		validateAttrs: function (attrs) {
+			var results = _.map(attrs, function (value, name) {
+				var validator = _.find(this.attrValidators, function (v) { return v.name === name; });
+				return validator ? validator.validateValue({ attr: name, path: name }, value) : null;
+			}, this);
+			results = _.filter(results, function (r) { return r != null; });
+			var isValid = results.length === 0;
+			return { isValid: isValid, results: results };
+		},
+		attr: function (name) {
+			var attrValidator = _.find(this.attrValidators, function (v) { return v.name === name; });
+			if (!attrValidator) {
+				attrValidator = new AttrValidator(name);
+				this.attrValidators.push(attrValidator);
+			}
+			return attrValidator;
+		}
+	});
+
+	// Validates a model attribute (also contains rule configuration functionality)
+	var AttrValidator = function (name) {
+		this.name = name;
+		this.rules = [];
 	};
 
 	_.extend(AttrValidator.prototype, {
 		validate: function (model) {
 			var value = model.get(this.name);
-			var errors = this.validateValue(value);
-			return errors;
+			return this.validateValue({ attr: this.name, path: this.name }, value);
 		},
-		validateValue: function (value) {
-			var ruleResults = _.map(this.rules, function (r) {
-				var isValid = r.isValid(value, { attrName: this.name });
-				return { isValid: isValid, message: r.message };
-			});
-			ruleResults = _.filter(ruleResults, function (r) { return r.isValid === false; });
-			return ruleResults.length > 0 ? { path: this.attr, errors: ruleResults} : null;
+		validateValue: function (context, value) {
+			var rule;
+			var errors = [];
+			for (var i = 0, l = this.rules.length; i < l; i++) {
+				rule = this.rules[i];
+				if (!rule.isValid(value)) {
+					var message = rule.options.message || messageBuilder.createMessage(rule.options, context);
+					errors.push({ message: message, key: rule.key });
+				}
+			}
+			return errors.length === 0 ? null : { attr: context.attr, path: context.path, errors: errors };
 		},
-		addRule: function (isValid, options) {
-			var rule = new Rule(isValid, options);
+		addRule: function (isValid, options, key) {
+			var rule = new Rule(isValid, options, key);
 			this.rules.push(rule);
 			return this;
 		},
 		// value cannot be null or undefined
-		notNull: function (message) {
+		notNull: function (options) {
 			return this.addRule(function (value) {
 				return !isNullOrUndefined(value);
-			}, { message: message, key: "notNull" });
+			}, options, "not-null");
+		},
+		// value cannot be null or undefined
+		range: function (options) {
+			var values = options.values || { };
+			return this.addRule(function (value) {
+				if (options.ignoreCase) {
+					return _.any(values, function (item) {
+						return areEqualIgnoreCase(value, item);
+					});
+				} else {
+					return _.include(values, value);
+				}
+			}, options, "range");
 		},
 		// non-empty string value
-		notBlank: function (message) {
+		notBlank: function (options) {
 			return this.addRule(function (value) {
 				return !isNullOrUndefined(value) && !_.isNull(value.toString().match(/\S/));
-			}, { message: message, key: "notNull" });
+			}, options, "string-not-blank");
 		},
 		// string value of specified min and max length
 		// min: minimum string length (inclusive) 
 		// max: maximum string length (inclusive) 
 		// trim: trim whitespace from start and end of value before testing length
-		length: function (options, message) {
+		length: function (options) {
 			return this.addRule(function (value) {
 				if (isNullOrUndefined(value)) {
 					return false;
 				}
-				value = options.trim ? trimString(value) : asString(value);
+				value = options.trim === true ? trimString(value) : asString(value);
 				return (!options.min || value.length >= options.min)
 					&& (!options.max || value.length <= options.max);
-			}, { message: message, key: "notNull" });
+			}, options, "string-length");
 		},
 		// custom check using a callback function. The function will be invoked with
 		// 2 arguments (value, context) - the context contains the name of the attr
 		// and the parent model if available
-		check: function (func, message) {
+		check: function (func, options) {
 			return this.addRule(function (value, context) {
 				return func(value, context);
-			}, { message: message, key: "check" });
+			}, options, "check");
 		}
 	});
 
+	// Individual validation rule
+	var Rule = function (isValid, options, key) {
+		this.isValid = isValid;
+		this.options = options || {};
+		this.key = key || "";
+	};
+
+
+	// Message display
+	var messageFormatUtility = {
+		singularOrPlural: function (value, singular, plural) {
+			return value == 1 ? singular : plural || singular + "s";
+		},
+		join: function (values, separator, lastSeparator) {
+			return values.length === 0 ? values : values.slice(0, values.length - 1).join(separator) + lastSeparator + values[values.length];
+		}
+	};
+
+	var MessageBuilder = function (config) {
+		this.templates = config.templates || {};
+		this.templates["default"] || (this.templates["default"] = "Please supply a valid value");
+		this.templateCache = {};
+	};
+
+	_.extend(MessageBuilder.prototype, {
+		createMessage: function (ruleOptions, context) {
+			var key = ruleOptions.key || "";
+			var item = this.templates[key] || this.templates["default"];
+			var templateContent = _.isFunction(item) ? item(ruleOptions) : item;
+			var template = this.templateCache["templateContent"] || (this.templateCache["templateContent"] = _.template(templateContent));
+			var data = _.extend({ options: ruleOptions, context: context }, messageFormatUtility);
+			return template(data);
+		}
+	});
+
+	var defaultMessageConfig = {
+		templates: {
+			"not-null": "Please supply a value",
+			"string-not-blank": "Please supply a value",
+			"string-length": function (options) {
+				var template = "Please supply a value";
+				if (options.min && options.max) {
+					template += " between <%= options.min %> and <%= options.max %> characters";
+				} else if (options.min) {
+					template += " of more than <%= options.min %> <%= singularOrPlural(options.min, 'character') %>";
+				} else if (options.max) {
+					template += " of less than <%= options.max %> <%= singularOrPlural(options.min, 'character') %>";
+				}
+				if (options.trim === true) {
+					template += " excluding whitespace at the start or end";
+				}
+				return template;
+			},
+			"range": function (options) {
+				var template = "Please supply a valid value (<%= join(options.values, ', ', ' or ') %>)";
+				if (options.ignoreCase === true) {
+					template += ", lower or upper case";
+				}
+				return template;
+			},
+			"default": "Please supply a valid value"
+		}
+	};
+
+	var messageBuilder = new MessageBuilder(defaultMessageConfig);
+
 	return {
-		ModelValidator: ModelValidator
+		ModelValidator: ModelValidator,
+		configureMessages: function (messageConfig) {
+			messageBuilder = new MessageBuilder(messageConfig);
+		}
 	};
 
 })();
--- spec/javascripts/validationspec.js
+++ spec/javascripts/validationspec.js
@@ -1,6 +1,6 @@
 describe("model validation", function () {
 	var f = JSON.stringify; // format objects in messages
-
+	var ModelValidator = Backbone.validation.ModelValidator;
 
 	// TODO - mixin 
 	describe("model extensions", function () {
@@ -44,8 +44,9 @@ describe("model validation", function () {
 			});
 
 			validator = new Backbone.validation.ModelValidator();
-			validator.attr("code").length({ min: 2, max: 5 }, "Between 2 and 5");
-			validator.attr("name").length({ min: 2, max: 5 }, "Between 2 and 5");
+			validator.attr("code").length({ min: 2, max: 5, message: "Between 2 and 5" });
+			validator.attr("name").length({ min: 2, max: 5, message: "Between 2 and 5" });
+			validator.attr("description").notNull({ message: "Not null" });
 		});
 
 		it("should be valid if single attr being tested is valid", function () {
@@ -63,18 +64,31 @@ describe("model validation", function () {
 			expect(result).toBeInvalid();
 		});
 
+		it("should describe each invalid attr", function () {
+			var result = validator.validateAttrs({ code: "123", name: "1", description: null });
+			expect(result.results).toEqual([ 
+				{ attr: "name", path: "name", errors: [ { message: "Between 2 and 5", key: "string-length" } ] },
+				{ attr: "description", path: "description", errors: [ { message: "Not null", key: "not-null" } ] } 
+			]);
+			
+		});
+
 	});
 
 
 	describe("nested object validation", function () {
 
-		var TestModel = Backbone.Model.extend();
+		var Person = Backbone.Model.extend();
 
-		var model, validator;
+		var dave, mary, validator;
 
 		beforeEach(function () {
 			validator = new Backbone.validation.ModelValidator();
-			model = new Person();
+			validator.attr("spouse").valid(function (child) {
+				child.attr("name")
+			});
+			dave = new Person({ firstName: "Dave", lastName: "Smith", gender: "male" });
+			mary = new Person({ firstName: "Mary", lastName: "Smith", gender: "female", spouse: dave });
 		});
 
 		describe("when validating nested model", function () {
@@ -111,23 +125,37 @@ describe("model validation", function () {
 					return result.isValid;
 
 				},
-				toBeInvalid: function () {
+				toBeInvalid: function (expectedMessages) {
 					model.set({ name: this.actual });
 					var result = validator.validate(model);
 					this.message = function () {
-						return "Expected model to be invalid but result was as follows: \n" + f(result);
+						var message = "Expected model to be invalid";
+						if (expectedMessages) {
+							message += " with messages: " + f(expectedMessages);
+						}
+						message += " but result was as follows: \n" + f(result);
+						return message;
 					};
-					return !result.isValid;
+					var messagesOk = true;
+					if (expectedMessages) {
+						if(result.results.length === 1) {
+							var actualMessages = _.pluck(result.results[0].errors, "message");
+							messagesOk = _.isEqual(actualMessages,expectedMessages);
+						} else {
+							messagesOk = false;
+						}
+					}
+					return !result.isValid && messagesOk;
 				}
 			});
 		});
 
 		describe("object attr rules", function () {
 
-			describe("not null", function () {
+			describe("not null with no custom message", function () {
 
 				beforeEach(function () {
-					validator.attr("name").notNull("Needs a name");
+					validator.attr("name").notNull();
 				});
 
 				it("null is invalid", function () {
@@ -139,7 +167,53 @@ describe("model validation", function () {
 				});
 
 				it("number is valid", function () {
-					expect(123).toBeValid(model);
+					expect(123).toBeValid();
+				});
+
+				it("should use default message", function () {
+					expect(null).toBeInvalid();
+				});
+			});
+
+			describe("range", function () {
+
+				beforeEach(function () {
+					validator.attr("name").range({values: ["male", "female"]});
+				});
+
+				it("null is invalid", function () {
+					expect(null).toBeInvalid();
+				});
+
+				it("expected values are valid", function () {
+					expect("male").toBeValid();
+					expect("female").toBeValid();
+				});
+
+				it("unspecified values are not valid", function () {
+					expect("Male").toBeInvalid();
+					expect("MALE").toBeInvalid();
+					expect("pending").toBeInvalid();
+					expect("tbc").toBeInvalid();
+				});
+			});
+
+			describe("range - ignore case", function () {
+
+				beforeEach(function () {
+					validator.attr("name").range({ values: ["male", "female"], ignoreCase: true });
+				});
+
+				it("expected values in any case are valid", function () {
+					expect("male").toBeValid();
+					expect("MALE").toBeValid();
+					expect("FEMALE").toBeValid();
+					expect("female").toBeValid();
+				});
+
+				it("unspecified values are not valid", function () {
+					expect("unsure").toBeInvalid();
+					expect("not bothered").toBeInvalid();
 				});
 			});
 
@@ -148,173 +222,173 @@ describe("model validation", function () {
 		describe("string attr rules", function () {
 			describe("not blank", function () {
 				beforeEach(function () {
-					validator.attr("name").notBlank("Needs a name");
+					validator.attr("name").notBlank();
 				});
 
 				it("null is invalid", function () {
-					expect(null).toBeInvalid(model);
+					expect(null).toBeInvalid();
 				});
 
 				it("empty string is invalid", function () {
-					expect("").toBeInvalid(model);
+					expect("").toBeInvalid();
 				});
 
 				it("spaces only is invalid", function () {
-					expect("   ").toBeInvalid(model);
+					expect("   ").toBeInvalid();
 				});
 
 				it("tabs only is invalid", function () {
-					expect("\t\t").toBeInvalid(model);
+					expect("\t\t").toBeInvalid();
 				});
 
 				it("non-empty string value is valid", function () {
-					expect("Dave").toBeValid(model);
+					expect("Dave").toBeValid();
 				});
 
 				it("number is valid", function () {
-					expect(123).toBeValid(model);
+					expect(123).toBeValid();
 				});
 			});
 
 			describe("length - min only", function () {
 
 				beforeEach(function () {
-					validator.attr("name").length({ min: 2 }, "Needs a name of at least 2 characters");
+					validator.attr("name").length({ min: 2 });
 				});
 
 				it("null is invalid", function () {
-					expect(null).toBeInvalid(model);
+					expect(null).toBeInvalid();
 				});
 
 				it("empty string is invalid", function () {
-					expect("").toBeInvalid(model);
+					expect("").toBeInvalid();
 				});
 
 				it("min length spaces only is valid", function () {
-					expect("  ").toBeValid(model);
+					expect("  ").toBeValid();
 				});
 
 				it("min length string value is valid", function () {
-					expect("ab").toBeValid(model);
+					expect("ab").toBeValid();
 				});
 
 				it("min length number is valid", function () {
-					expect(12).toBeValid(model);
+					expect(12).toBeValid();
 				});
 
 				it("below min length number is invalid", function () {
-					expect(1).toBeInvalid(model);
+					expect(1).toBeInvalid();
 				});
 			});
 
 			describe("length - max only", function () {
 
 				beforeEach(function () {
-					validator.attr("name").length({ max: 5 }, "Needs a name of up to 5 characters");
+					validator.attr("name").length({ max: 5 });
 				});
 
 				it("null is invalid", function () {
-					expect(null).toBeInvalid(model);
+					expect(null).toBeInvalid();
 				});
 
 				it("empty string is valid", function () {
-					expect("").toBeValid(model);
+					expect("").toBeValid();
 				});
 
 				it("below max length spaces only is valid", function () {
-					expect("     ").toBeValid(model);
+					expect("     ").toBeValid();
 				});
 
 				it("string above max length is invalid", function () {
-					expect("Daveasdfasf").toBeInvalid(model);
+					expect("Daveasdfasf").toBeInvalid();
 				});
 
 				it("number below max length is valid", function () {
-					expect(12345).toBeValid(model);
+					expect(12345).toBeValid();
 				});
 
 				it("number above max length is invalid", function () {
-					expect(123456).toBeInvalid(model);
+					expect(123456).toBeInvalid();
 				});
 			});
 
 			describe("length - min only trimmed", function () {
 
 				beforeEach(function () {
-					validator.attr("name").length({ min: 2, trim: true }, "Needs a name of at least 2 characters");
+					validator.attr("name").length({ min: 2, trim: true });
 				});
 
 				it("empty string is invalid", function () {
-					expect("").toBeInvalid(model);
+					expect("").toBeInvalid();
 				});
 
 				it("min length spaces only is invalid", function () {
-					expect("  ").toBeInvalid(model);
+					expect("  ").toBeInvalid();
 				});
 
 				it("min length tabs only is invalid", function () {
-					expect("\t\t").toBeInvalid(model);
+					expect("\t\t").toBeInvalid();
 				});
 
 				it("insufficient length when trimmed is invalid", function () {
-					expect("  A  ").toBeInvalid(model);
+					expect("  A  ").toBeInvalid();
 				});
 
 				it("min length string value is valid", function () {
-					expect("ab").toBeValid(model);
+					expect("ab").toBeValid();
 				});
 
 				it("min length number is valid", function () {
-					expect(12).toBeValid(model);
+					expect(12).toBeValid();
 				});
 			});
 
 			describe("length - min and max trimmed", function () {
 
 				beforeEach(function () {
-					validator.attr("name").length({ min: 2, max: 5, trim: true }, "Needs a name of at least 2 characters");
+					validator.attr("name").length({ min: 2, max: 5, trim: true });
 				});
 
 				it("empty string is invalid", function () {
-					expect("").toBeInvalid(model);
+					expect("").toBeInvalid();
 				});
 
 				it("min length spaces only is invalid", function () {
-					expect("  ").toBeInvalid(model);
+					expect("  ").toBeInvalid();
 				});
 
 				it("min length tabs only is invalid", function () {
-					expect("\t\t").toBeInvalid(model);
+					expect("\t\t").toBeInvalid();
 				});
 
 				it("below min length when trimmed is invalid", function () {
-					expect("  A  ").toBeInvalid(model);
-					expect("     A").toBeInvalid(model);
-					expect("A    ").toBeInvalid(model);
+					expect("  A  ").toBeInvalid();
+					expect("     A").toBeInvalid();
+					expect("A    ").toBeInvalid();
 				});
 
 				it("above max length when trimmed is invalid", function () {
-					expect("  AAAAAA  ").toBeInvalid(model);
-					expect("     AAAAAAA").toBeInvalid(model);
-					expect("AAAAAAA    ").toBeInvalid(model);
+					expect("  AAAAAA  ").toBeInvalid();
+					expect("     AAAAAAA").toBeInvalid();
+					expect("AAAAAAA    ").toBeInvalid();
 				});
 
 				it("within min and max when trimmed is valid", function () {
-					expect("  AAAA   ").toBeValid(model);
+					expect("  AAAA   ").toBeValid();
 				});
 
 				it("within min and max is valid", function () {
-					expect("ab").toBeValid(model);
-					expect("abc").toBeValid(model);
-					expect("abcd").toBeValid(model);
-					expect("abcde").toBeValid(model);
+					expect("ab").toBeValid();
+					expect("abc").toBeValid();
+					expect("abcd").toBeValid();
+					expect("abcde").toBeValid();
 				});
 
 				it("number within min and max is valid", function () {
-					expect(12).toBeValid(model);
-					expect(123).toBeValid(model);
-					expect(1234).toBeValid(model);
-					expect(12345).toBeValid(model);
+					expect(12).toBeValid();
+					expect(123).toBeValid();
+					expect(1234).toBeValid();
+					expect(12345).toBeValid();
 				});
 			});
 
@@ -323,15 +397,15 @@ describe("model validation", function () {
 				beforeEach(function () {
 					validator.attr("name").check(function (value, context) {
 						return value.toString().indexOf("monkey") != -1;
-					}, "Needs to contain the word monkey");
+					}, { message: "Needs to contain the word monkey" });
 				});
 
 				it("value not matching rule is invalid", function () {
-					expect("asdf").toBeInvalid(model);
+					expect("asdf").toBeInvalid();
 				});
 
 				it("value matching rule is valid", function () {
-					expect("monkey man").toBeValid(model);
+					expect("monkey man").toBeValid();
 				});
 			});
 		});
