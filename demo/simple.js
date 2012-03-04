cc48dde3e48fc75464929a79a99db2868f217355
Dan Malcolm <danmalc@gmail.com>
2012-03-04 23:37:41 +0000

    Updating demos

--- demo/simple.js
+++ demo/simple.js
@@ -1,147 +1,167 @@
-﻿var Model = Backbone.Model.extend(Backbone.validation.modelValidation);
+﻿$(function () {
 
-var rules = Backbone.validation.rules;
+	// alias Backbone.validation.rules to make rules definition on our models more concise
+	var rules = Backbone.validation.rules;
 
-var User = Model.extend({
-	rules: {
-		email: rules.email({ trim: true }),
-		password: rules.length({ trim: true, min: 6, max: 20 })
-	},
-	sync: function (method, model, options) {
-		var resp = { id: 1234 };
-		_.extend(resp, this.attributes);
-		options.success(resp);
-	}
-});
+	var Model = Backbone.Model.extend(Backbone.validation.modelValidation);
 
-var ErrorsView = Backbone.View.extend({
-	initialize: function () {
-		this.invalidValues = [];
-		this.$el.hide();
-	},
-	template: _.template($('#validation-messages').html()),
-	update: function (invalidValues) {
-		this.invalidValues = invalidValues || [];
-		this.render();
-	},
-	render: function () {
-		if (this.invalidValues.length > 0) {
-			var $messages = this.$(".messages");
-			$messages.html(this.template({ invalidValues: this.invalidValues }));
-			this.$el.show(300);
-		} else {
+	var User = Model.extend({
+		// Define validation rules
+		rules: {
+			email: rules.email({ trim: true }),
+			password: rules.length({ trim: true, min: 6, max: 20 })
+		},
+		// Pretend to save to server
+		sync: function (method, model, options) {
+			var resp = { id: 1234 };
+			_.extend(resp, this.attributes);
+			options.success(resp);
+		}
+	});
+
+	// Displays summary of validation errors
+	var ErrorsView = Backbone.View.extend({
+		initialize: function () {
 			this.$el.hide();
-		}
-		return this;
-	}
-});
-
-// Some
-
-var SimpleValidationView = Backbone.View.extend({
-
-	initialize: function () {
-		this.errorsView = new ErrorsView({ el: this.$(".errors") });
-	},
-	events: {
-		"change [name=email]": "updateEmail",
-		"change [name=password]": "updatePassword",
-		"click input:submit": "save"
-	},
-	save: function (e) {
-		e.preventDefault();
-		var result = this.model.validate();
-		if (!result || result.isValid) {
-			this.model.save(null, { success: function () {
-				alert("Well done, you have registered!");
+		},
+		template: _.template($('#validation-messages').html()),
+		clear: function () {
+			this.update();
+		},
+		update: function (invalidValues) {
+			if (invalidValues && invalidValues.length > 0) {
+				var $messages = this.$(".messages");
+				$messages.html(this.template({ invalidValues: invalidValues }));
+				this.$el.show();
+			} else {
+				this.$el.hide();
 			}
-			});
+			return this;
 		}
-		else {
-			this.errorsView.update(result.invalidValues);
-		}
-	},
-	updateEmail: function (ev) {
-		this.updateModel({ email: $(ev.target).val() });
-	},
-	updatePassword: function (ev) {
-		this.updateModel({ password: $(ev.target).val() });
-	},
-	updateModel: function (attrs) {
-		this.model.set(attrs, { silent: true });
-	}
-});
+	});
 
-var InteractiveValidationView = Backbone.View.extend({
-	initialize: function () {
-		this.errorsView = new ErrorsView({ el: this.$(".errors") });
-		// Records attributes that user has updated - this is used
-		// to control whether we show validation errors for values
-		// that haven't been entered yet
-		this.attemptedAttributes = {};
-		this.attemptedSave = false;
-		this.model.on("error", this.error, this);
-	},
-	events: {
-		"change input[name='email']": "updateEmail",
-		"keyup input[name=password]": "updatePassword",
-		"click input:submit": "save"
-	},
-	save: function (e) {
-		e.preventDefault();
-		this.attemptedSave = true;
-		if (this.validateModel()) {
-			this.model.save({
-				success: function () {
-					alert("Well done, you have registered!");
+	// Simple form example. We attempt to update all attributes when the user
+	// submits the form and display summary of validation messages at top of
+	// form if validation fails.
+	var SimpleValidationView = Backbone.View.extend({
+
+		initialize: function () {
+			this.errorsView = new ErrorsView({ el: this.$(".errors") });
+			// Attempting to set invalid attributes results in error
+			// being triggered
+			this.model.on("error", this.error, this);
+		},
+		events: {
+			"click input:submit": "save"
+		},
+		save: function (e) {
+			e.preventDefault();
+
+			var attrs = {
+				email: this.$("input[name=email]").val(),
+				password: this.$("input[name=password]").val()
+			};
+			var self = this;
+			this.model.save(attrs, {
+				success: function (model) {
+					self.errorsView.clear();
+					alert("Well done, you have registered!" + JSON.stringify(model.attributes));
 				}
 			});
+		},
+		// Fired when invalid attributes are set
+		error: function (model, result) {
+			if (result && result.invalidValues) {
+				this.errorsView.update(result.invalidValues);
+			}
 		}
-	},
-	updateEmail: function (ev) {
-		this.updateModel({ email: $(ev.target).val() });
-	},
-	updatePassword: function (ev) {
-		this.updateModel({ password: $(ev.target).val() });
-	},
-	updateModel: function (attrs) {
-		for (var key in attrs) {
-			this.attemptedAttributes[key] = true;
-		}
-		this.model.set(attrs, { silent: true });
-		this.validateModel();
-	},
-	validateModel: function () {
-		var result = this.model.validate();
-		var invalidValues;
-		if (result) {
-			invalidValues = _.filter(result.invalidValues, function (value) {
-				return this.attemptedSave || this.attemptedAttributes[value.attr] === true;
-			}, this);
-			this.enableSave(false);
-		} else {
-			invalidValues = [];
-			this.enableSave(true);
-		}
-		this.errorsView.update(invalidValues);
-	},
-	enableSave: function (enabled) {
-		if (enabled) {
-			this.$("input:submit").removeAttr("disabled").attr("title", "Register using the values supplied");
-		}
-		else {
-			this.$("input:submit").attr("disabled", "disabled").attr("title", "Please enter all the required information before submitting the form");
-		}
-	},
-	render: function () {
-		this.enableSave(this.model.isValid());
-		return this;
-	}
-});
+	});
 
+	// Displays validation errors interactively as user updates fields in the
+	// form. This is more challenging:
+	// 1. By default, Backbone.Model does not allow attributes to get into 
+	// an invalid state. If we're updating individual attributes interactively
+	// as they're updated, wediff between form and model.
+	var InteractiveValidationView = Backbone.View.extend({
+		initialize: function () {
+			this.errorsView = new ErrorsView({ el: this.$(".errors") });
+			// Records attributes that user has updated - this is used
+			// to prevent form from showing validation errors for values
+			// that haven't been entered yet
+			this.attemptedAttributes = {};
+			// Once user has attempted to save, we show validation errors
+			// for all attributes
+			this.attemptedSave = false;
+		},
+		events: {
+			"change input[name=email]": "updateEmail",
+			// provide feedback as password is typed
+			"keyup input[name=password]": "updatePassword",
+			"click input:submit": "save"
+		},
+		updateEmail: function (ev) {
+			this.updateModel({ email: $(ev.target).val() });
+		},
+		updatePassword: function (ev) {
+			this.updateModel({ password: $(ev.target).val() });
+		},
+		updateModel: function (attrs) {
+			for (var key in attrs) {
+				this.attemptedAttributes[key] = true;
+			}
+			// Update attributes silently so model is in sync with form
+			this.model.set(attrs, { silent: true });
+			// Validate the entire model
+			this.validateModel();
+		},
+		validateModel: function () {
+			var result = this.model.validate();
+			if (!result) {
+				// Model.validate returns nothing if there are no errors
+				this.errorsView.clear();
+				this.enableSave(true);
+				return true;
+			} else {
+				var invalidValues = result.invalidValues;
+				if (!this.attemptedSave) {
+					invalidValues = _.filter(result.invalidValues, function (value) {
+						return this.attemptedAttributes[value.attr] === true;
+					}, this);
+				}
+				this.errorsView.update(invalidValues);
+				this.enableSave(false);
+				return false;
+			}
+		},
+		enableSave: function (enabled) {
+			if (enabled) {
+				this.$("input:submit").removeAttr("disabled").attr("title", "Register using the values supplied");
+			}
+			else {
+				this.$("input:submit").attr("disabled", "disabled").attr("title", "Please enter all the required information before submitting the form");
+			}
+		},
+		save: function (e) {
+			e.preventDefault();
+			// It now makes sense to display errors for all attributes
+			this.attemptedSave = true;
+			if (this.validateModel()) {
+				var self = this;
+				this.model.save(null, {
+					success: function (model) {
+						self.errorsView.clear();
+						alert("Well done, you have registered!" + JSON.stringify(model.attributes));
+					}
+				});
+			}
+		},
+		render: function () {
+			this.enableSave(this.model.isValid());
+			return this;
+		}
+	});
 
-$(function () {
-
+	// Start things up
 	new SimpleValidationView({
 		el: "#simple",
 		model: new User({ email: "", password: "" })
